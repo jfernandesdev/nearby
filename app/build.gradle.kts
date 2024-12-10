e6daf7884d8342aadba0435d741ef9aa95e3e4e8
Jeferson Fernandes <jfernandes.dev@gmail.com>
2024-12-09 22:09:22 -0300

    FEAT: import color, typography and icon resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.jfernandesdev.nearby"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.jfernandesdev.nearby"
